548a29571a3edcda924a18717f212ccdf21060e9
unknown <evievi@outlook.com>
2020-04-08 11:53:10 +0200

    Initial serach form (no function yet)

--- main.php
+++ main.php
@@ -1,15 +1,24 @@
-
-<?php require "connection.php"; ?>
-
-<!-- HTML form to upload images -->
 <!DOCTYPE html>
 <html>
 <body>
 
+<h1>Search for animals</h1>
+<!-- require that connection to database works -->
+<?php require "connection.php"; ?> 
+
+<!-- HTML form to upload images -->
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
+</form>
+
+<!-- HTML form to search for an animal-->
+<form action="query.php" method="post">
+Select an animal: <input type="text" name="name"><br> 
+or <br>
+Search by name: <select name="name"><br>
+<input type="submit">
 </form>
 
 </body>
