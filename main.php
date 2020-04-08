832d989bdbe1f090c0877b2846d56c5253fd2104
unknown <evievi@outlook.com>
2020-04-08 18:21:46 +0200

    Moved select dropdown to a separate php file.

--- main.php
+++ main.php
@@ -13,30 +13,8 @@
     <input type="submit" value="Upload Image" name="submit">
 </form>
 
-<?php
-//Our select statement. This will retrieve the data that we want.
-$sql = "SELECT * FROM animals";
-
-//Prepare the select statement.
-$stmt = $db->prepare($sql);
-
-//Execute the statement.
-$stmt->execute();
-
-//Retrieve the rows using fetchAll.
-$animals = $stmt->fetchAll();
-?>
-
 <!-- HTML form to search for an animal-->
-<form action="query.php" method="post">
-Select an animal:
-<select>
-    <option value=0>Select</option>
-    <?php foreach($animals as $animal){
-    echo  "<option value= ". $animal['id']. ">". $animal['name']. "</option>";
-    }
-    ?>
-</select> <br>
+<?php include 'select.php'; ?>
 or <br>
 Search by name: <input type="text" name="name"><br> 
 <input type="submit" value="Search">
---
+++ select.php
@@ -0,0 +1,24 @@
+<?php
+//This statement will retrieve the data that we want.
+$sql = "SELECT * FROM animals";
+
+//Prepare the select statement.
+$stmt = $db->prepare($sql);
+
+//Execute the statement.
+$stmt->execute();
+
+//Retrieve the rows using fetchAll.
+$animals = $stmt->fetchAll();
+?>
+
+<!-- HTML form to search for an animal-->
+<form action="query.php" method="post">
+Select an animal:
+<select>
+    <option value=0>Select</option>
+    <?php foreach($animals as $animal){
+    echo  "<option value= ". $animal['id']. ">". $animal['name']. "</option>";
+    }
+    ?>
+</select> <br>
